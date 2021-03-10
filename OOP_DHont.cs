f5ede85156740dd7d1439850f121256c28d04e37
Thomas Jay Pollicott <25312638@students.lincoln.ac.uk>
2021-03-10 19:07:27 +0000

    declaration of class and objects for main

---
+++ OOP_DHont.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_DHont
+{
+    class politicalParty
+    {
+        public string partyName { get; set; }
+        public int initialVotes { get; private set; }
+        public int subsequentVotes { get; set; }
+        public int partySeats { get; set; }
+
+        public politicalParty(string name, int votes)
+        {
+            partyName = name;
+            initialVotes = votes;
+            subsequentVotes = votes;
+        }
+
+        public string printParty()
+        {
+            return $"Party Name: {partyName}  Initial Votes: {initialVotes}";
+        }
+        
+
+        public void DHontMethod()
+        {
+            subsequentVotes = initialVotes / (partySeats + 1);
+        }
+
+
+    }
+
+
+
+}
